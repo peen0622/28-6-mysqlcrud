b8f20e4581ee5124c1b8c0be201874112d704174
smart-09 <smart-09@smart-09-PC>
2018-06-26 14:24:17 +0900

    [김호순]

---
+++ src/service/StudentAddr.java
@@ -0,0 +1,26 @@
+//28±â ±èÈ£¼ø 2018.6.26(È­)
+package service;
+
+public class StudentAddr {
+	private int  studentAddrNo;
+	private int studentNo;
+	private String StudentAddrContent;
+	public int getStudentAddrNo() {
+		return studentAddrNo;
+	}
+	public void setStudentAddrNo(int studentAddrNo) {
+		this.studentAddrNo = studentAddrNo;
+	}
+	public int getStudentNo() {
+		return studentNo;
+	}
+	public void setStudentNo(int studentNo) {
+		this.studentNo = studentNo;
+	}
+	public String getStudentAddrContent() {
+		return StudentAddrContent;
+	}
+	public void setStudentAddrContent(String studentAddrContent) {
+		StudentAddrContent = studentAddrContent;
+	}
+}
